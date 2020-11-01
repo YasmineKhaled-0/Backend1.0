e8878a54118cdb75c01718950b12248196bea436
Yasmine <62294339+YasmineKhaled-0@users.noreply.github.com>
2020-11-01 15:43:43 +0200

    Add files via upload

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = new Faker;
+        foreach (range(0, 20) as $number) {
+            Category::create([
+                'name' => Str::random(5),
+                'parent_id' => rand(1, count(Category::all())),
+                'description' => Str::random(50)
+            ]);
+        }
+
+
+    }
+}
