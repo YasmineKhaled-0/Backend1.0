8ac9902fabe0d2fd80b7ddab06a14928ecbc7aa7
Yasmine <62294339+YasmineKhaled-0@users.noreply.github.com>
2020-11-01 15:47:39 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function (){
+    return 'INDEX';
+});
+
+Route::group([
+    'prefix' => '/admin',
+    'as' => 'admin.',
+    'middleware' => 'auth'
+], function () {
+
+    Route::get('/', 'DashboardController')->name('dashboard');
+    Route::resource('/categories', 'CategoryController');
+    Route::get('/categories/export', 'CategoryController@export')->name('categories.export');
+    Route::resource('/products', 'ProductController');
+    Route::resource('/orders', 'OrderController');
+    Route::resource('/clients', 'ClientController');
+
+    Route::group([
+        'prefix' => 'clients',
+        'as' => 'clients.'
+    ], function () {
+        Route::get('/get-clients', 'ClientController@getClients')->name('get-clients');
+    });
+    Route::resource('/clients', 'ClientController');
+
+    Route::get('/products', 'ProductController@index')->name('products.index');
+    Route::get('/products/{product}', 'ProductController@show')->name('products.show');
+});
+
+
+
+
+Auth::routes();
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+// Client Auth Routes
+
+Route::get('/clients/register', 'Auth\RegisterController@clientRegisterForm')->name('client-register-form');
+Route::post('/clients/register', 'Auth\RegisterController@registerClient')->name('register-client');
+Route::get('/clients/login', 'Auth\LoginController@clientLoginForm')->name('client-login-form');
+Route::post('/clients/login', 'Auth\LoginController@clientLogin')->name('client-login');
+Route::get('/clients/logout', 'Auth\LoginController@clientLogout')->name('client-logout');
